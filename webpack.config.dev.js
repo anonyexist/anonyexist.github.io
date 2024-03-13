63b04d83d3b2a48dfb8a8d2fd1524c6955885d31
anonyexist <163063037+anonyexist@users.noreply.github.com>
2024-03-13 23:20:28 +0000

    commit

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./src/webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
